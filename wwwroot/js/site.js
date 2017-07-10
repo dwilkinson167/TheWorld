9d291f4c9f3dc3589288b65d94d90de94bf8dcc3
Dylan Wilkinson <dylanwilkinson@gmail.com>
2017-07-10 12:53:52 -0700

    More js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,11 @@
-﻿
+﻿var ele = document.getElementById("username");
+ele.innerHTML = "Dylan Wilkinson"
+
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+    main.style.backgroundColor = "#888";
+};
+
+main.onmouseleave = function () {
+    main.style.backgroundColor = "";
+};
