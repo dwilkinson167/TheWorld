12b6b5207e41be923cd9393542d936e6949af825
Dylan Wilkinson <dylanwilkinson@gmail.com>
2017-07-11 16:42:01 -0700

    begin Entity objects

--- Models/Trip.cs
+++ Models/Trip.cs
@@ -8,6 +8,13 @@ namespace TheWorld.Models
 {
     public class Trip
     {
-        
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string UserName { get; set; }
+
+        public ICollection<Stop> Stops { get; set; }
+
     }
+
 }
