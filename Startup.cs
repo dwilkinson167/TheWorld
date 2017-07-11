63f7f5431b59cb94e86cd1c4eb5e0e905d32b849
Dylan Wilkinson <dylanwilkinson@gmail.com>
2017-07-10 19:47:38 -0700

    Added view+controller

--- Startup.cs
+++ Startup.cs
@@ -17,14 +17,24 @@ namespace TheWorld
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseDefaultFiles();
 
             app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                  name: "Default",
+                  template: "{controller}/{action}/{id?}",
+                  defaults: new { Controller = "App", action = "Index" }
+                  );
+
+            });
                 
             
         }
