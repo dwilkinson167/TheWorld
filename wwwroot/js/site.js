2b3110d3f6d635a7776a8be81c6f5ce700dca5b3
Dylan Wilkinson <dylanwilkinson@gmail.com>
2017-07-10 19:18:45 -0700

    Added Usefull JQuery for sidebar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,22 +1,32 @@
 ﻿(function () {
 
 
-    var ele = $("#username");
-    ele.innerHTML = "Dylan Wilkinson"
+//    var ele = $("#username");
+//    ele.innerHTML = "Dylan Wilkinson"
 
-    var main = $("#main");
-    main.on("mouseenter", function () {
-        main.style = "backgroundColor = #888;";
-    });
+//    var main = $("#main");
+//    main.on("mouseenter", function () {
+//        main.style = "backgroundColor = #888;";
+//    });
 
-    main.on("mouseleave", function () {
-        main.style = "";
-    });
+//    main.on("mouseleave", function () {
+//        main.style = "";
+//    });
 
-    var menuItems = $("ul.menu li a");
-    menuItems.on("click", function () {
-        var me = $(this);
-        alert(me.text());
+//    var menuItems = $("ul.menu li a");
+//    menuItems.on("click", function () {
+//        var me = $(this);
+//        alert(me.text());
 
-    });
+//    });
+
+
+
+
+var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+$("#sidebarToggle").on("click", function (){
+    $sidebarAndWrapper.toggleClass("hide-sidebar");
+
+});
 })();
